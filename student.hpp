0ed7e3d8543d43801e2d6502a1578cf20e3c8f20
n1ckname152 <vnrishisaran@gmail.com>
2020-09-28 23:30:20 -0700

    added all fields according to requirements

--- student.hpp
+++ student.hpp
@@ -1,3 +1,31 @@
-//header file student.hpp to declare your classes
-using namespace std; //use namespace std
-#include <string> //you will have to use string in C++
+// header file student.hpp to declare your classes
+using namespace std;  // use namespace std
+#include <string>     //you will have to use string in C++
+class Student {
+ private:
+  string firstName;
+  string lastName;
+  float cgpa;
+  int researchScore;
+  int applicationID;
+};
+
+class DomesticStudent : public Student {
+ private:
+  string province;
+};
+
+class InternationalStudent : public Student {
+ private:
+  string country;
+  ToeflScore toefl;
+};
+
+class ToeflScore {
+ private:
+  int reading;
+  int listening;
+  int speaking;
+  int writing;
+  int total;
+};
